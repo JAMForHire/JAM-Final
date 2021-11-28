0621986973cdf102c881041646761f36d0c3e79f
bhavesh42 <bajajb@rpi.edu>
2021-11-27 21:58:44 -0500

    Profile Menu
    Implemented basic profile menu design (toggled from nav bar)

---
+++ scripts/profile.js
@@ -0,0 +1,6 @@
+function toggleProfile() {
+  if(document.getElementById("profile-menu").style.display == "none") {
+    document.getElementById("profile-menu").style.display = "flex";
+  }
+  else document.getElementById("profile-menu").style.display = "none";
+}
