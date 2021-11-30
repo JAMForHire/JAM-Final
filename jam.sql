7d97c664ccf9d0d0c5d1fbe49630f31da2e7dba6
bhavesh42 <bajajb@rpi.edu>
2021-11-30 09:37:29 -0500

    Fixed SQL
    Fixed SQL to add back users table and properly add foreign key

--- jam.sql
+++ jam.sql
@@ -1,17 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Nov 30, 2021 at 04:55 AM
--- Server version: 5.7.30
--- PHP Version: 7.4.9
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2021 at 03:36 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: `JAM`
+-- Database: `jam`
 --
 
 -- --------------------------------------------------------
@@ -37,6 +44,31 @@ CREATE TABLE `jars` (
 INSERT INTO `jars` (`id`, `user_id`, `company`, `date`, `notes`, `link`, `progress`) VALUES
 (1, 0, 'RPI', '2021-11-08', 'Job opportunity at RPI', 'https://www.rpi.edu/', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` bigint(20) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `type` int(1) NOT NULL,
+  `filepath` varchar(100) NOT NULL,
+  `dob` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `fname`, `lname`, `type`, `filepath`, `dob`) VALUES
+(0, 'username', '$2y$10$Md2KwwqOJ1YhXuzDqqBWQeLy0BQKRtMC5cI3B5drpubStTZs0YL7G', 'fname', 'lname', 0, 'uploads/26.png', '2021-11-16'),
+(24, 'h', '$2y$10$fLSfLOMCyFpOqA.4eukmhe7dVFiUcqjPmda23KTPCLfow7kRXZkWu', 'h', 'h', 0, 'uploads/24.jpg', '0000-00-00');
+
 --
 -- Indexes for dumped tables
 --
@@ -49,6 +81,12 @@ ALTER TABLE `jars`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -57,3 +95,24 @@ ALTER TABLE `jars`
 --
 ALTER TABLE `jars`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jars`
+--
+ALTER TABLE `jars`
+  ADD CONSTRAINT `jars_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
