0ebf8108463d33d30da42b1305a934d856db5c8f
bhavesh42 <bajajb@rpi.edu>
2021-12-10 15:07:05 -0500

    Fixed export

--- export.sql
+++ export.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2021 at 02:35 PM
+-- Generation Time: Dec 10, 2021 at 09:06 PM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -48,8 +48,9 @@ INSERT INTO `jars` (`id`, `user_id`, `company`, `date`, `notes`, `link`, `progre
 (10, 26, 'Amazon', '2021-12-22', 'Amazon full time position', 'https://www.amazon.jobs/en/teams/internships-for-students', 3, 0),
 (15, 26, 'Amazon', '2021-12-22', 'Amazon full time position', 'https://www.amazon.jobs/en/teams/internships-for-students', 3, 1),
 (20, 26, 'Amazon', '2021-12-22', 'Amazon full time position', 'https://www.amazon.jobs/en/teams/internships-for-students', 3, 1),
-(21, 26, 'Amazon', '2021-12-22', 'Amazon full time position', 'https://www.amazon.jobs/en/teams/internships-for-students', 3, 1),
-(22, 0, 'Amazon', '2021-12-14', 'Job opportunity at AWS', 'https://www.amazon.jobs/en/teams/internships-for-students', 3, 0);
+(21, 26, 'Amazon', '2021-12-22', 'Amazon full time position', 'https://www.amazon.jobs/en/teams/internships-for-students', 3, 0),
+(22, 0, 'Amazon', '2021-12-14', 'Job opportunity at AWS', 'https://www.amazon.jobs/en/teams/internships-for-students', 3, 0),
+(23, 26, 'test jar 4', '2021-12-15', 'a', 'a', 3, 0);
 
 -- --------------------------------------------------------
 
@@ -75,7 +76,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `fname`, `lname`, `type`, `filepath`, `dob`, `resume`) VALUES
 (0, 'username', '$2y$10$Md2KwwqOJ1YhXuzDqqBWQeLy0BQKRtMC5cI3B5drpubStTZs0YL7G', 'john', 'smith', 0, 'uploads/26.png', '2021-11-16', 'Resumes/0.docx'),
-(26, 'jsmith', '$2y$10$FK7cDkrX4Dk5cqf3VSsZ6.76huphIeFP1qUdpOxE8FiIXIs3RJJZ6', 'John', 'Smith', 0, 'uploads/26.png', '2021-12-23', NULL),
+(26, 'jsmith', '$2y$10$FK7cDkrX4Dk5cqf3VSsZ6.76huphIeFP1qUdpOxE8FiIXIs3RJJZ6', 'John', 'Smith', 0, 'uploads/26.png', '2021-12-23', 'Resumes/26.pdf'),
 (27, 'h', '$2y$10$54epIpEh5avjh2GADvZqpeXKvBF4tXDBms3wSCRKB07InTF6Oysma', 'h', 'h', 0, 'uploads/27.jpg', NULL, NULL);
 
 --
@@ -103,7 +104,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `jars`
 --
 ALTER TABLE `jars`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `users`
